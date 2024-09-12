a946bbb1fa9383a99db91effd8ef8c75fadfe028
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-09-12 11:03:40 -0400

    Improve license write

--- src/views/licenses/license-write/license-add.component.ts
+++ src/views/licenses/license-write/license-add.component.ts
@@ -23,7 +23,7 @@ export class LicenseAddComponent extends LicenseWriteComponent implements OnInit
   }
 
   protected override onSubmit(license: License): Observable<License> {
-    return this.apiConnector.createLicense(license, this.activatedRoute.snapshot.queryParamMap.get('organizationId'));
+    return this.apiConnector.createLicense(license, this.activatedRoute.snapshot.queryParams['organizationId']);
   }
 
 }
--- src/views/licenses/license-write/license-write.component.ts
+++ src/views/licenses/license-write/license-write.component.ts
@@ -15,7 +15,7 @@ export abstract class LicenseWriteComponent extends LicenseContentComponent {
 
     submit(): void {
         const license = { ...this.formGroup.value };
-        this.onSubmit(license).pipe(take(1), tap(() => this.router.navigate(['/licenses'], { queryParams: this.activatedRoute.snapshot.queryParams }))).subscribe();
+        this.onSubmit(license).pipe(take(1), tap(() => this.router.navigate(['/licenses'], { queryParams: { organizationId: this.activatedRoute.snapshot.queryParams['organizationId'] } }))).subscribe();
     }
 
     protected abstract onSubmit(license: License): Observable<License>;
