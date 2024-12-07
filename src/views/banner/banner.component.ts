82eca164b6a02dd5c955da7d39140c509fe4c454
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-12-07 00:07:07 -0500

    Improve banner component

--- src/views/banner/banner.component.ts
+++ src/views/banner/banner.component.ts
@@ -2,22 +2,19 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter, map } from 'rxjs';
 import { AuthenticationManager } from '../../managers/authentication.manager';
+import { AuthenticationAwareComponent } from '../../modules/authentication-aware/authentication-aware.component';
+import { AuthenticationAwareContainer } from '../../modules/authentication-aware/authentication-aware.container';
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrl: './banner.component.css'
+	selector: 'app-banner',
+	templateUrl: './banner.component.html',
+	styleUrl: './banner.component.css'
 })
-export class BannerComponent {
-
-  private readonly url$ = this.router.events.pipe(filter(e => e instanceof NavigationEnd), map(e => (e as NavigationEnd).url));
-
-  readonly userProfile$ = this.authenticationManager.userProfile$;
-  readonly isLoginPage$ = this.url$.pipe(map(url => url === '/login'));
-
-  constructor(
-    private readonly authenticationManager: AuthenticationManager,
-    private readonly router: Router) {
-  }
+export class BannerComponent extends AuthenticationAwareContainer {
+	constructor(
+		protected override readonly authenticationManager: AuthenticationManager,
+		protected override readonly router: Router) {
+		super(authenticationManager, router);
+	}
 
 }
