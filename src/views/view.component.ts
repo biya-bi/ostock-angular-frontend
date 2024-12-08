56ebc9558fb9d48b7cd63363af8696cfbf8028a1
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-12-08 11:01:54 -0500

    Make view component extend base component

--- src/views/view.component.ts
+++ src/views/view.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Params } from '@angular/router';
+import { BaseComponent } from './base.component';
 
 @Component({
     template: '',
 })
-export abstract class ViewComponent {
+export abstract class ViewComponent extends BaseComponent {
 
     @Input() queryParams: Params;
 
