efd692e7b474d665c3b41f5088f74de0199717c5
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-12-23 02:51:17 -0500

    Remove obsolete properties

--- src/views/entity-list-view.component.ts
+++ src/views/entity-list-view.component.ts
@@ -1,19 +1,16 @@
 import { Component, EventEmitter, Output, QueryList, ViewChildren } from '@angular/core';
+import { EntityContext } from '../contexts/entity.context';
 import { SearchCriteria } from '../criteria/search-criteria';
 import { SearchEvent } from '../events/search.event';
 import { SortEvent } from '../events/sort.event';
-import { Operation } from '../models/operation';
 import { NgbdSortableHeader } from '../modules/sort/sortable.directive';
 import { EntityComponent } from './entity.component';
-import { EntityContext } from '../contexts/entity.context';
 
 @Component({
   template: '',
   standalone: false
 })
 export abstract class EntityListViewComponent<T, U extends EntityContext<T, SearchCriteria>, V extends SearchEvent<SearchCriteria>> extends EntityComponent<T, U> {
-
-  operation: Operation;
 
   @Output() search = new EventEmitter<V>();
   @Output() pageChange = new EventEmitter<number>();
