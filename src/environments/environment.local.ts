9f73e799258d6d1c6e7a9f66f2a8754541d7b6ff
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2025-08-13 19:42:03 -0400

    Update local API connector url

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -3,7 +3,7 @@ import { Environment } from '../models/environment';
 export const environment: Environment = {
     production: false,
     vapidPublicKey: '',
-    apiConnectorUrl: 'http://localhost:3000/api',
+    apiConnectorUrl: 'http://gateway-service.ostock/api',
     keycloakConfig: {
         issuer: 'http://keycloak.infra/realms/ostock',
         requireHttps: false
